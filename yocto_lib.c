dcd2491c2cb60485317d71a7877ec2f7dcc67815
Tony <48407231+NaikanLinux@users.noreply.github.com>
2023-10-28 07:38:29 +0200

    Create yocto_lib.c
    Create this file to test from Yocto recipe

---
+++ yocto_lib.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+#include <string.h>
+
+#include "yocto_test.h"
+
+void LibHelloWorld (
+) {
+  printf_tm("Hello World (from a shared library!)\n");
+}
